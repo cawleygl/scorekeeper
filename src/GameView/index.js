e8607d0fd75bd6bd385e08062d7d1631d88edab0
Grant Cawley <grantlcawley@gmail.com>
2023-03-23 14:15:34 -0400

    scorecard styling

--- src/GameView/index.js
+++ src/GameView/index.js
@@ -1,9 +1,23 @@
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+import { Row, Col, Container, Table } from 'react-bootstrap';
 
-function GameView({ gameData, teamData }) {
-  const cardLength = 200;
+function GameView({ gameData, homeTeam, roadTeam }) {
+
+  const scorecardData = homeTeam.roster;
+
+  const grid = scorecardData.map((data, index) =>
+    <tr key={index}>
+      <th className="cardBox" >{data}</th>
+      <th className="abContainer">
+        <div id="ball1" className="countBox" />
+        <div id="ball2" className="countBox" />
+        <div id="ball3" className="countBox" />
+        <div id="strike1" className="countBox" />
+        <div id="strike2" className="countBox" />
+        <div className="bases" />
+        <div className="result">8-6-4-2-6-7-5-5-5-4</div>
+      </th>
+    </tr>
+  );
 
   return (
     <>
@@ -11,12 +25,28 @@ function GameView({ gameData, teamData }) {
         <Row>
           <h1>GAME VIEW</h1>
         </Row>
-        {/* {posts.map((post) => */}
-          <Row>
-            <Col>
-            </Col>
-          </Row>
-        {/* )} */}
+        <Table hover striped>
+          <thead>
+            <tr>
+              <th className="numBox" width={200}></th>
+              <th className="numBox" >1</th>
+              <th className="numBox" >2</th>
+              <th className="numBox" >3</th>
+              <th className="numBox" >4</th>
+              <th className="numBox" >5</th>
+              <th className="numBox" >6</th>
+              <th className="numBox" >7</th>
+              <th className="numBox" >8</th>
+              <th className="numBox" >9</th>
+              <th className="numBox" >Stats</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {grid}
+          </tbody>
+        </Table>
+
       </Container>
     </>
 
