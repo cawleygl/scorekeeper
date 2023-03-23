d629730c29f0d39b6809ab1f6fbc6d83461f5e18
Grant Cawley <grantlcawley@gmail.com>
2023-03-23 14:16:29 -0400

    import sample data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
 import { Container } from 'react-bootstrap';
 import GameView from './GameView';
 
+import sampleData from './sampleData.json';
+
 
 function App() {
+
+
+  var gameData = sampleData.games[0];
+  var homeTeam = sampleData.teams[0];
+  var roadTeam = sampleData.teams[1];
+
+
   return (
     <Container>
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<GameView />} />
+        <Route exact path="/" element={<GameView gameData={gameData} homeTeam={homeTeam} roadTeam={roadTeam} />} />
       </Routes>
     </BrowserRouter>
   </Container>
