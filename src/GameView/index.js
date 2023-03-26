400e408ee009e06ad82cf5fb752a1f27932b25c1
Grant Cawley <grantlcawley@gmail.com>
2023-03-26 19:53:04 -0400

    render scorecard from basic data using css

--- src/GameView/index.js
+++ src/GameView/index.js
@@ -2,52 +2,165 @@ import { Row, Col, Container, Table } from 'react-bootstrap';
 
 function GameView({ gameData, homeTeam, roadTeam }) {
 
-  console.log(gameData.scoreRecord);
+  const homePlayers = homeTeam.lineup;
 
-  const grid = gameData.scoreRecord.slice(0, 1).map((data, index) =>
-    <tr key={index}>
-      <th>
-        {data.players.map((player) =>
-          <div>{player.num}</div>
-        )}
-      </th>
+  console.log("ScoreRecord:", gameData.scoreRecord);
+  console.log("Home Team:", homePlayers);
 
-      <th>
-        {data.players.map((player) =>
-          <div>{player.name}</div>
-        )}
-      </th>
-      <th>
-        {data.players.map((player) =>
-          <div>{player.pos}</div>
-        )}
-      </th>
+  function inningCol(data, index) {
 
-      <th id="inning1" className="abContainer">
-        <div id="ball1" className={data.ballCount >= 1 ? "countBox active" : "countBox" } />
-        <div id="ball2" className={data.ballCount >= 2 ? "countBox active" : "countBox" } />
-        <div id="ball3" className={data.ballCount >= 3 ? "countBox active" : "countBox" } />
-        <div id="strike1" className={data.strikeCount >= 1 ? "countBox active" : "countBox" } />
-        <div id="strike2" className={data.strikeCount >= 2 ? "countBox active" : "countBox" } />
-        
-        <div className="bases" />
-        <div className="result">{data.result}</div>
-        {data.out ? <div className="out">{data.outNum}</div> : null}
-        {data.out && data.outNum === 3 ? <div className="inningChange"></div> : null}
+    const baseRender = (totalBases, outOnBases, reachBase) => {
+      if (!reachBase || !totalBases) return <div className="bases empty" ></div>
+
+      var basesClass = "";
+      var markerClass = "";
+
+      switch (totalBases) {
+        case 1:
+          if (outOnBases) {
+            markerClass = "secondBaseOutMarker";
+            basesClass = "outlineToSecond";
+          } else {
+            basesClass = "outlineToFirst";
+          }
+          break;
+        case 2:
+          if (outOnBases) {
+            markerClass = "thirdBaseOutMarker";
+            basesClass = "outlineToThird";
+          } else {
+            basesClass = "outlineToSecond";
+          }
+          break;
+        case 3:
+          if (outOnBases) {
+            markerClass = "homePlateOutMarker";
+            basesClass = "outlineToHome";
+          } else {
+            basesClass = "outlineToThird";
+          }
+          break;
+        case 4:
+          basesClass = "fill";
+          if (outOnBases) console.warn("Unknown Base Configuration:\n***\n Out On Bases and 4 Total Bases\n***");
+          break;
+        default:
+          console.warn("Unknown Base Configuration:\n***\nReached Base: ", reachBase, "\nTotal Bases: ", totalBases, "\nOut On Bases: ", outOnBases + "\n***\n");
+          break;
+      }
+
+      return <>
+        <div className={"bases " + basesClass} />
+        {outOnBases ? <div className={"outMarker " + markerClass}>x</div> : null}
+      </>
+
+    }
+
+    return <th>
+      <div className="abContainer">
+
+        <div className={data.ballCount >= 1 ? "countBox ball1 active" : "countBox ball1"} />
+        <div className={data.ballCount >= 2 ? "countBox ball2 active" : "countBox ball2"} />
+        <div className={data.ballCount >= 3 ? "countBox ball3 active" : "countBox ball3"} />
+        <div className={data.strikeCount >= 1 ? "countBox strike1 active" : "countBox strike1"} />
+        <div className={data.strikeCount >= 2 ? "countBox strike2 active" : "countBox strike2"} />
+
+        {data.firstBaseResult ? <div className="firstBaseResult">{data.firstBaseResult}</div> : null}
+        {data.secondBaseResult ? <div className="secondBaseResult">{data.secondBaseResult}</div> : null}
+        {data.thirdBaseResult ? <div className="thirdBaseResult">{data.thirdBaseResult}</div> : null}
+        {data.homeResult ? <div className="homeResult">{data.homeResult}</div> : null}
+
+        {baseRender(data.totalBases, data.outOnBases, data.reachBase,)}
+
+        {!data.reachBase && data.outResult ? <div className="outResult">{data.outResult}</div> : null}
+        {(!data.reachBase || data.outOnBases) && data.outNum ? <div className="out">{data.outNum}</div> : null}
+        {(!data.reachBase || data.outOnBases) && data.outNum === 3 ? <div className="inningChange"></div> : null}
+
+      </div>
+    </th>
+
+  }
 
 
-      </th>
-    </tr>
+  function tableRow(data, index) {
+    let innings = data.abData;
+    console.log("Innings: ", innings);
+
+    if (data.abData.length < 9) {
+      console.log("Incomplete Grid:", innings.length, 9-innings.length);
+
+      let addInnings = new Array(9 - data.abData.length);
+
+      addInnings.fill(
+        {
+          "inning": 0,
+          "ballCount": 0,
+          "strikeCount": 0,
+
+          "reachBase": false,
+          "outOnBases": false,
+          "outNum": 0,
+
+          "totalBases": 0,
+
+          "outResult": "",
+          "firstBaseResult": "",
+          "secondBaseResult": "",
+          "thirdBaseResult": "",
+          "homeResult": ""
+        }
+
+      );
+      innings = innings.concat(addInnings);
+    }
+
+    const inningsRender = innings.map((ab, index) => inningCol(ab, index));
+
+    return <>
+      <tr key={index}>
+
+        {/* Player Numbers Mapped from Roster */}
+        <th>
+          {data.playerIDs.map((playerID) =>
+            <div>{homePlayers.find(player => playerID === player.id).num}</div>
+          )}
+        </th>
+
+        {/* Player Names Mapped from Roster */}
+        <th>
+          {data.playerIDs.map((playerID) =>
+            <div>{homePlayers.find(player => playerID === player.id).name}</div>
+          )}
+        </th>
+
+        {/* Player Position from Game Data */}
+        <th>
+          {data.playerPos.map((Pos) =>
+            <div>{Pos}</div>
+          )}
+        </th>
+
+        {inningsRender}
+
+      </tr>
+
+    </>
+  }
+
+  const gameGrid = gameData.scoreRecord.map((data, index) =>
+    <>
+      {tableRow(data, index)}
+    </>
   );
 
-  
+
   return (
     <>
       <Container fluid>
         <Row>
           <h1>GAME VIEW</h1>
         </Row>
-        <Table hover bordered striped>
+        <Table id="gameViewTable" hover bordered striped>
           <thead>
             <tr>
               <th width={20}>#</th>
@@ -70,7 +183,7 @@ function GameView({ gameData, homeTeam, roadTeam }) {
           </thead>
 
           <tbody>
-            {grid}
+            {gameGrid}
           </tbody>
         </Table>
 
