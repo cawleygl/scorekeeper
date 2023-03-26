c83a46b44405814ee981e48c81dd6103710b2fb8
Grant Cawley <grantlcawley@gmail.com>
2023-03-25 23:11:43 -0400

    added dynamic score view

--- src/GameView/index.js
+++ src/GameView/index.js
@@ -2,19 +2,39 @@ import { Row, Col, Container, Table } from 'react-bootstrap';
 
 function GameView({ gameData, homeTeam, roadTeam }) {
 
-  const scorecardData = homeTeam.roster;
+  console.log(gameData.scoreRecord);
 
-  const grid = scorecardData.map((data, index) =>
+  const grid = gameData.scoreRecord.slice(0, 1).map((data, index) =>
     <tr key={index}>
-      <th className="cardBox" >{data}</th>
-      <th className="abContainer">
-        <div id="ball1" className="countBox" />
-        <div id="ball2" className="countBox" />
-        <div id="ball3" className="countBox" />
-        <div id="strike1" className="countBox" />
-        <div id="strike2" className="countBox" />
+      <th>
+        {data.players.map((player) =>
+          <div>{player.num}</div>
+        )}
+      </th>
+
+      <th>
+        {data.players.map((player) =>
+          <div>{player.name}</div>
+        )}
+      </th>
+      <th>
+        {data.players.map((player) =>
+          <div>{player.pos}</div>
+        )}
+      </th>
+
+      <th id="inning1" className="abContainer">
+        <div id="ball1" className={data.ballCount >= 1 ? "countBox active" : "countBox" } />
+        <div id="ball2" className={data.ballCount >= 2 ? "countBox active" : "countBox" } />
+        <div id="ball3" className={data.ballCount >= 3 ? "countBox active" : "countBox" } />
+        <div id="strike1" className={data.strikeCount >= 1 ? "countBox active" : "countBox" } />
+        <div id="strike2" className={data.strikeCount >= 2 ? "countBox active" : "countBox" } />
         <div className="bases" />
-        <div className="result">8-6-4-2-6-7-5-5-5-4</div>
+        <div className="result">{data.result}</div>
+        {data.out ? <div className="out">{data.outNum}</div> : null}
+        {data.out && data.outNum === 3 ? <div className="inningChange"></div> : null}
+
+
       </th>
     </tr>
   );
@@ -25,20 +45,25 @@ function GameView({ gameData, homeTeam, roadTeam }) {
         <Row>
           <h1>GAME VIEW</h1>
         </Row>
-        <Table hover striped>
+        <Table hover bordered striped>
           <thead>
             <tr>
-              <th className="numBox" width={200}></th>
-              <th className="numBox" >1</th>
-              <th className="numBox" >2</th>
-              <th className="numBox" >3</th>
-              <th className="numBox" >4</th>
-              <th className="numBox" >5</th>
-              <th className="numBox" >6</th>
-              <th className="numBox" >7</th>
-              <th className="numBox" >8</th>
-              <th className="numBox" >9</th>
-              <th className="numBox" >Stats</th>
+              <th width={20}>#</th>
+              <th width={175}></th>
+              <th width={20}>POS</th>
+              <th>1</th>
+              <th>2</th>
+              <th>3</th>
+              <th>4</th>
+              <th>5</th>
+              <th>6</th>
+              <th>7</th>
+              <th>8</th>
+              <th>9</th>
+              <th width={20} >AB</th>
+              <th width={20} >R</th>
+              <th width={20} >H</th>
+              <th width={20} >RBI</th>
             </tr>
           </thead>
 
